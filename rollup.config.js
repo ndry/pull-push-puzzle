0a65be7c5f796064623c6ec64cc2dbf1f1ed72ce
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2022-05-29 19:23:22 +0300

    megred prod and dev config

--- rollup.config.js
+++ rollup.config.js
@@ -5,38 +5,31 @@ import typescript from '@rollup/plugin-typescript';
 import { packages } from './package-lock.json';
 import _package from './package.json';
 
-const isDev = process.env.NODE_ENV?.trim() !== 'production';
-
 const importmap = (() => {
     const version = (name) => packages["node_modules/" + name].version;
     const nameAtVesrion = name => /.@/.test(name) ? name : `${name}@${version(name)}`;
     const unpkg = path => name => `https://unpkg.com/${nameAtVesrion(name)}/${path}`;
     const esmBundle = path => name => `https://unpkg.com/@esm-bundle/${nameAtVesrion(name)}/${path}`;
-    const min = isDev ? "" : ".min";
     return {
         "imports": _.mapValues({
             "lodash": esmBundle(`esm/index.js`),
             "rxjs": esmBundle(`esm/es2015/rxjs.min.js`),
             "preact": unpkg(`dist/preact.mjs`),
             "preact/hooks": unpkg(`hooks/dist/hooks.mjs`)("preact"),
-            "@emotion/css": esmBundle(`esm/emotion${min}.js`)("emotion@10.0.27"),
+            "@emotion/css": esmBundle(`esm/emotion.min.js`)("emotion@10.0.27"),
         }, (link, name) => _.isFunction(link) ? link(name) : link),
     }
 })();
 
-const renderIndexHtml = (bundle) => {
-    const importmapStr = JSON.stringify(importmap, undefined, isDev ? "    " : undefined);
-    const modulePreloadStr = Object.values(importmap.imports)
-        .map(link => /*html*/`<link rel="modulepreload" href="${link}"/>`)
-        .join("\n");
-    return /*html*/`<!DOCTYPE html>
-<html lang="en"><head><title>${_package.name}</title></head><body>
+const renderIndexHtml = (bundle) => /*html*/`<!DOCTYPE html><html lang="en">
+<head><title>${_package.name}</title></head>
+<body>
     <script async src="https://unpkg.com/es-module-shims@1.5.5/dist/es-module-shims.js"></script>
-    <script type="importmap">${importmapStr}</script>
-    ${modulePreloadStr}
+    <script type="importmap">${JSON.stringify(importmap)}</script>
+    ${Object.values(importmap.imports).map(x => /*html*/`<link rel="modulepreload" href="${x}"/>`).join("\n")}
     <script type="module">${bundle}</script>
-</body></html>`;
-}
+</body>
+</html>`;
 
 const bundleName = `index.bundle.js`;
 export default {
@@ -44,7 +37,7 @@ export default {
     output: {
         file: bundleName,
         format: 'esm',
-        sourcemap: isDev && "inline",
+        sourcemap: 'inline',
     },
     external: Object.keys(importmap.imports),
     plugins: [
@@ -53,9 +46,7 @@ export default {
             browser: true,
         }),
         commonjs(),
-        typescript(isDev && {
-            target: "ESNext",
-        }),
+        typescript(),
         {
             generateBundle(outputOptions, bundleInfo) {
                 this.emitFile({
